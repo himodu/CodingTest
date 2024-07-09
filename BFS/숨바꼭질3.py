86a446609d22b7dedfc0383dbd7f8ac64f76cf60
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-07-09 22:10:03 +0900

    Create 숨바꼭질3.py

---
+++ BFS/숨바꼭질3.py
@@ -0,0 +1,44 @@
+from  collections import deque
+
+s, e=map(int, input().split())
+
+field = [-1]*100001
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    field[start] = 0
+    while queue:
+        current = queue.popleft()
+        for i in range(3):
+            if i == 0:
+                if current==0:
+                    continue
+                next = current*2
+            if i == 1:
+                next = current+1
+            if i == 2:
+                next = current-1
+
+            if next <= -1 or next >= 100001:
+                continue
+
+            if field[next]==-1:
+                if not i==0:
+                    field[next] = field[current]+1
+                    queue.append(next)
+                else:
+                    field[next] = field[current]
+                    queue.append(next)
+            else:
+                if not i==0:
+                    if field[next] > field[current]+1:
+                        field[next] = field[current]+1
+                        queue.append(next)
+                else:
+                    if field[next] > field[current]:
+                        field[next] = field[current]
+                        queue.append(next)
+
+bfs(s)
+print(field[e])
