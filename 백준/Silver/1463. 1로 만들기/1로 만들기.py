270fba739766677f9e8d863719a02745f603fe13
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-07-20 15:53:30 +0900

    [Silver III] Title: 1로 만들기, Time: 620 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+dp = [-1]*(n+1)
+
+dp[1] = 0
+
+for i in range(2, n+1):
+    min = 1000000
+    if dp[i//2] != -1 and i%2==0:
+        if min > dp[i//2]:
+            min = dp[i//2]
+    if dp[i//3] != -1 and i%3==0:
+        if min > dp[i//3]:
+            min = dp[i // 3]
+
+    if min > dp[i-1]:
+        min = dp[i-1]
+
+    dp[i] = min + 1
+
+print(dp[n])
