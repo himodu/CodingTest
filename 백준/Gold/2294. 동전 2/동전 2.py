303b0fc2bf268746a1ac4e342f3aa2f6435241e5
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-07-24 20:20:14 +0900

    [Gold V] Title: 동전 2, Time: 236 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n, k=map(int, input().split())
+
+dp = [-1]*(k+1)
+dp[0] = 0
+coins = []
+for i in range(n):
+    coins.append(int(input()))
+
+
+queue = deque()
+queue.append(0)
+while queue:
+    current = queue.popleft()
+    if current == k:
+        break
+    for i in range(n):
+        next = current + coins[i]
+        if  next<=k and dp[next] == -1:
+            dp[next] = dp[current] + 1
+            queue.append(current+coins[i])
+
+print(dp[k])
