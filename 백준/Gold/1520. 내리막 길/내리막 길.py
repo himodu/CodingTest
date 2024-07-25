d88f0f20b96ce0669aac1749e4c701dd64ea5c15
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-07-25 13:42:53 +0900

    [Gold III] Title: 내리막 길, Time: 160 ms, Memory: 42788 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,31 @@
+n,m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+dp = [[-1]*m for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1,0 ,-1]
+
+def dfs(x,y):
+    if x==n-1 and y==m-1:
+        return 1
+
+    if dp[x][y] != -1:
+        return dp[x][y]
+
+    ways = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<m:
+            if graph[x][y] > graph[nx][ny]:
+                ways += dfs(nx,ny)
+
+    dp[x][y] = ways
+    return dp[x][y]
+
+print(dfs(0,0))
+
