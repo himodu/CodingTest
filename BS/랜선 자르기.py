4573dd781feb6071cc9982be6c5767e6a1994377
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-07-29 20:59:32 +0900

    Create 랜선 자르기.py

---
+++ BS/랜선 자르기.py
@@ -0,0 +1,26 @@
+k, n = map(int, input().split())
+lines = []
+max = 0
+for i in range(k):
+    line = int(input())
+    lines.append(line)
+    if line > max:
+        max = line
+start = 1
+end = max
+result = 0
+while end >= start:
+    count = 0
+    index = (start + end) // 2
+
+    for i in range(k):
+        able = lines[i] // index
+        count += able
+
+    if count < n:
+        end = index-1
+    else:
+        result = index
+        start = index+1
+        
+print(result)
