553a198829b63353f304b30c7d5292432de09b21
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-08-14 21:38:11 +0900

    [Gold V] Title: 암호 만들기, Time: 196 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,41 @@
+l, c = map(int, input().split())
+
+chars = list(input().split())
+chars.sort()
+
+answer = []
+
+def ismot(chars):
+    mot = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for i in range(len(chars)):
+        for j in range(len(mot)):
+            if chars[i]==mot[j]:
+                count += 1
+    return count
+
+def track(index):
+
+    length = len(answer)
+    mot = ismot(answer)
+    if index == c:
+        if length == l and mot >= 1 and length-mot >= 2:
+            for i in range(length):
+                print(answer[i], end='')
+            print()
+            return
+        return
+
+    if length == l and mot >= 1 and length-mot >= 2:
+        for i in range(length):
+            print(answer[i], end='')
+        print()
+        return
+
+    answer.append(chars[index])
+    track(index+1)
+
+    answer.pop()
+    track(index+1)
+
+track(0)
