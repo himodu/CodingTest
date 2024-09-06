a9ceb62a772e8ab016943d5e6a05e8b04be7fcfb
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-09-06 20:02:23 +0900

    [Gold III] Title: ACM Craft, Time: 1212 ms, Memory: 140480 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -3,21 +3,17 @@ from collections import deque
 def bfs(graph,time,goal,isStart):
 
     queue = deque()
-    visited = [False] * (len(time) + 1)
     stackTime = time.copy()
 
     for i in range(len(isStart)):
         if isStart[i]:
             queue.append(i+1)
-            visited[i+1] = True
 
     while queue:
         node = queue.popleft()
-
         for i in graph[node]:
-            if not visited[i] or stackTime[i-1] < stackTime[node-1]+time[i-1]:
+            if stackTime[i-1] < stackTime[node-1]+time[i-1]:
                     stackTime[i-1] = stackTime[node-1]+time[i-1]
-                    visited[i] = True
                     queue.append(i)
 
     return stackTime[goal-1]
@@ -33,7 +29,6 @@ for i in range(t):
     time = list(map(int, input().split()))
 
     isStart = [True]*n
-
     for j in range(k):
         start, end = map(int, input().split())
         isStart[end-1] = False
